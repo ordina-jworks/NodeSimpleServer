71732515a968536d882ea0afa3e4014712387b7e
Kevin Van den Abeele <kevin.vandenabeele@ordina.be>
2018-04-30 07:43:37 +0200

    Upgraded dependencies

---
+++ resources/config/config.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Config {
+    constructor() {
+        //Config variables:
+        this.settings = {
+            httpPort: 8080,
+            socketPort: 8000,
+            webContentFolder: "resources/www",
+            endpointScanFolder: "src/web/endpoints/impl/",
+            allowFolderListing: true,
+            intervalTimeoutInSeconds: 480,
+            defaultCacheSize: 250
+        };
+        this.arduino = {
+            enableArduino: false,
+            useSerialOverJohnnyFive: false,
+            serialPortName: "usbmodem",
+            serialPortBaudRate: 57600
+        };
+        this.keys = {};
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //!!!!!!!! Do NOT use the standard users when deploying! For test purposes only !!!!!!!!!!!
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        this.roles = {
+            admin: 'admin',
+            user: 'user'
+        };
+        this.auth = {
+            users: [
+                { user: 'admin', auth: 'YWRtaW46YWRtaW4=', roles: [this.roles.admin, this.roles.user] },
+                { user: 'user', auth: 'dXNlcjp1c2Vy', roles: [this.roles.user] }
+            ]
+        };
+        //Empty private constructor.
+    }
+}
+Config.getInstance = () => {
+    if (!Config.instance) {
+        Config.instance = new Config();
+    }
+    return Config.instance;
+};
+exports.Config = Config;
+//# sourceMappingURL=config.js.map
---
+++ resources/test-config/test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sinonChai = require("sinon-chai");
+const chai = require("chai");
+chai.use(sinonChai);
+//# sourceMappingURL=test.js.map
